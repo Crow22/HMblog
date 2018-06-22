12b6e4bbd2ff05ee0c86b55929c060f559829370
Crow22 <youmustbekidding@163.com>
2018-06-22 16:09:07 +0800

    first commit

---
+++ src/main/java/com/homesickmonkey/hmblog/HmblogApplication.java
@@ -0,0 +1,12 @@
+package com.homesickmonkey.hmblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HmblogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HmblogApplication.class, args);
+    }
+}
---
+++ src/test/java/com/homesickmonkey/hmblog/HmblogApplicationTests.java
@@ -0,0 +1,16 @@
+package com.homesickmonkey.hmblog;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HmblogApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
